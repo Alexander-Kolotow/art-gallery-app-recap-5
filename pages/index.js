09b672e41db851be1af32f628953ae6994a943dc
Julian Klett <julian22a@googlemail.com>
2023-11-27 15:13:50 +0100

    solve US1

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,36 @@
+import useSWR from "swr";
+import { ArtPieces } from "../components/ArtPieces/ArtPieces.js";
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+
+  // If the status code is not in the range 200-299,
+  // we still try to parse and throw it.
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    // Attach extra info to the error object.
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
+
 export default function HomePage() {
+  const {
+    data: pictures,
+    error,
+    isLoading,
+  } = useSWR("https://example-apis.vercel.app/api/art", fetcher);
+  if (error) return <div>{error.message}</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  console.log(pictures);
+
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <>
+      <ArtPieces pieces={pictures}></ArtPieces>
+    </>
   );
 }

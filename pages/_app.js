94c1edc28efbc8f78a2977c1b113acca5e0659b7
Nirand Widera <nirand-widera@web.de>
2023-11-28 12:25:24 +0100

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,59 @@
 import GlobalStyle from "../styles";
+import { useEffect, useState } from "react";
+import { SWRConfig } from "swr";
+import useSWR from "swr";
+import { Layout } from "@/components/Layout/Layout";
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+
+  // If the status code is not in the range 200-299,
+  // we still try to parse and throw it.
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    // Attach extra info to the error object.
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
 
 export default function App({ Component, pageProps }) {
+  const {
+    data: pictures,
+    error,
+    isLoading,
+  } = useSWR("https://example-apis.vercel.app/api/art", fetcher);
+  if (error) return <div>{error.message}</div>;
+  if (isLoading) return <div>loading...</div>;
+  console.log(pictures);
   return (
-    <>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </>
+    <Layout>
+      <SWRConfig
+        value={{
+          fetcher,
+        }}
+      >
+        <GlobalStyle />
+        <Component {...pageProps} pictures={pictures} />
+      </SWRConfig>
+    </Layout>
   );
 }
+
+/* function MyApp({ Component, pageProps }) {
+  const [artPieces, setArtPieces] = useState([]);
+
+  useEffect(() => {
+    // Your data fetching logic goes here
+    // Example: fetch('/api/art-pieces').then(response => setArtPieces(response));
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ artPieces, setArtPieces }}>
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  );
+} */
--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,17 @@
-import useSWR from "swr";
 import { ArtPieces } from "../components/ArtPieces/ArtPieces.js";
 import { Spotlight } from "../components/Spotlight/Spotlight.js";
-import Link from "next/link.js";
 
-const fetcher = async (url) => {
-  const res = await fetch(url);
-
-  // If the status code is not in the range 200-299,
-  // we still try to parse and throw it.
-  if (!res.ok) {
-    const error = new Error("An error occurred while fetching the data.");
-    // Attach extra info to the error object.
-    error.info = await res.json();
-    error.status = res.status;
-    throw error;
-  }
-
-  return res.json();
-};
-
-export default function HomePage() {
-  const {
-    data: pictures,
-    error,
-    isLoading,
-  } = useSWR("https://example-apis.vercel.app/api/art", fetcher);
-  if (error) return <div>{error.message}</div>;
-  if (isLoading) return <div>loading...</div>;
-
+export default function SpotlightPage({ pictures }) {
   //
   const randomIndex = Math.floor(Math.random() * pictures.length);
   const randomPicture = pictures[randomIndex];
 
   return (
     <>
-      <ArtPieces pieces={pictures}/>
-      <Spotlight image={randomPicture.imageSource}
-            artist={randomPicture.artist}/>
+      <Spotlight
+        image={randomPicture.imageSource}
+        artist={randomPicture.artist}
+      />
     </>
   );
 }

d9490ae792d32132b06c0c210bfc643a0cce514a
Alexander-Kolotow <147614805+Alexander-Kolotow@users.noreply.github.com>
2023-11-30 20:18:11 +0000

    testing Favorite Button

--- components/FavoriteButton/FavoriteButton.js
+++ components/FavoriteButton/FavoriteButton.js
@@ -1,8 +1,11 @@
 export function FavoriteButton({ handleToggleFavorite, isFavorite, slug }) {
 
+  // in der Konsole testen, ob er sich den slug ziehen kann und ob die Funktion definiert ist.
   const handleClick = () => {
+    console.log("handleToggleFavorite:", handleToggleFavorite);
+    console.log("slug:", slug);
     handleToggleFavorite(slug);
-  };
+  }; 
 
   return (
     <button onClick={handleClick}>
